582ab735d4b17cad72e47ade01d341a573191712
John Tyree <johntyree@gmail.com>
2014-08-02 00:15:10 -0500

    Applicify everything and fill out number tower

--- Main.hs
+++ Main.hs
@@ -2,11 +2,15 @@
 
 module Main where
 
+import Control.Applicative
 import Control.Monad
 import Data.Char
+import Data.Complex
+import Data.Maybe
+import Data.Ratio
 import Numeric
 import System.Environment
-import Text.ParserCombinators.Parsec hiding (spaces)
+import Text.ParserCombinators.Parsec hiding (spaces, many, (<|>), optional)
 
 
 main :: IO ()
@@ -17,7 +21,7 @@ main = do
 
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 
 spaces :: Parser ()
@@ -28,10 +32,14 @@ data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
              | Number Integer
+             | Float Double
+             | Rational (Ratio Integer)
+             | Complex (Complex Double)
              | String String
              | Bool Bool
              | Character String
     deriving Show
+
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
@@ -39,86 +47,101 @@ readExpr input = case parse parseExpr "lisp" input of
     Right val -> "Found value: " ++ show val
 
 
+parseBool :: Parser LispVal
+parseBool = Bool . (== 't') <$> (char '#' *> oneOf "ft")
+
+
 parseString :: Parser LispVal
-parseString = do
-    char '"'
-    x <- many (escapedOrNot "nrt\\\"")
-    char '"'
-    return $ String x
+parseString = String <$> between quote quote escChars
+  where
+    quote = char '"'
+    escChars = many (escapedOrNot "nrt\\\"")
 
 
 escapedOrNot :: String -> Parser Char
-escapedOrNot escChars = escaped <|> (noneOf "\"")
-    where escaped = char '\\' >> oneOf escChars >>= readChar
-          readChar c = return . read $ "'\\" ++ [c] ++ "'"
+escapedOrNot escChars = readChar <$> (escaped <|> noneOf "\"")
+  where
+    escaped = char '\\' *> oneOf escChars
+    readChar c = read $ "'\\" ++ [c] ++ "'"
 
 
 parseCharacter :: Parser LispVal
-parseCharacter = do
-    try $ char '#' >> char '\\'
-    rest <- many $ satisfy (not . isSpace)
-    return $ Character rest
+parseCharacter = (Character . readChar) <$> (prefix *> many nonWhitespace)
+    where prefix = try $ char '#' *> char '\\'
+          nonWhitespace = satisfy (not . isSpace)
+          readChar [c] = return . read $ "'\\" ++ [c] ++ "'"
+          readChar s = s
 
 
 parseAtom :: Parser LispVal
-parseAtom = do
-    first <- letter <|> symbol
-    rest <- many (letter <|> digit <|> symbol)
-    let atom = first:rest
-    return $ case atom of
-        "#t" -> Bool True
-        "#f" -> Bool False
-        _    -> Atom atom
+parseAtom = Atom <$> atom
+  where
+    first = letter <|> symbol
+    rest = many (letter <|> digit <|> symbol)
+    atom = (:) <$> first <*> rest
 
-
--- parseFloat :: Parser LispVal
--- parseFloat = do
-    -- [(raw, suffix)] <- readFloat
-    -- if
 
 parseDecimalNumber :: Parser LispVal
-parseDecimalNumber = liftM (Number . read) $ many1 digit
-
-parseOctalNumber :: Parser LispVal
-parseOctalNumber = do
-    try $ char '#' >> char 'o'
-    n <- many1 (oneOf "01234567")
-    let [(val, _)] = readOct n
-    return (Number val)
+parseDecimalNumber = Number . read <$> (optional (string "#d") *> many1 digit)
 
 parseBinaryNumber :: Parser LispVal
-parseBinaryNumber = do
-    try $ char '#' >> char 'b'
-    n <- many1 (oneOf "01")
-    let [(val, _)] = readBin n
-    return (Number val)
+parseBinaryNumber = parseRadixNumber "01" readBin
   where
     readBin = readInt 2 (`elem` "01") (read . return)
 
+parseOctalNumber :: Parser LispVal
+parseOctalNumber = parseRadixNumber "01234567" readOct
+
 parseHexNumber :: Parser LispVal
-parseHexNumber = do
-    try $ char '#' >> char 'x'
-    n <- many1 (oneOf "0123456789abcdefABCDEF")
-    let [(val, _)] = readHex n
-    return (Number val)
+parseHexNumber = parseRadixNumber "0123456789abcdefABCDEF" readHex
+
+parseRadixNumber :: String -> ReadS Integer -> Parser LispVal
+parseRadixNumber chars decimalize = try $ prefix *> fmap numberfy digits
+  where
+    prefix = char '#' *> char 'o'
+    numberfy = Number . fst . head . decimalize
+    digits = many1 (oneOf chars)
 
 parseNumber :: Parser LispVal
-parseNumber = parseOctalNumber
-          <|> parseHexNumber
-          <|> parseBinaryNumber
-          <|> parseDecimalNumber
+parseNumber = try (parseOctalNumber
+               <|> parseHexNumber
+               <|> parseBinaryNumber
+               <|> parseDecimalNumber)
 
-parseNumber' :: Parser LispVal
-parseNumber' = do
-    digits <- many1 digit
-    return . Number . read $ digits
+parseFloat :: Parser LispVal
+parseFloat = fmap (Float . fst . head . readFloat) parseFloatString
 
-parseNumber'' :: Parser LispVal
-parseNumber'' = many1 digit >>= return . Number. read
+parseFloatString :: Parser String
+parseFloatString = try $ do
+    beginning <- many digit <* char '.'
+    end <- many digit
+    guard $ (not . null) (beginning ++ end)
+    -- We can slap extra 0's in there to avoid ".###" and "###.".
+    return $ "0" ++ beginning ++ "." ++ end ++ "0"
+
+parseRational :: Parser LispVal
+parseRational = try $ numberfy <$> int <* over <*> int
+  where
+    int = many1 digit
+    over = ws <* char '/' <* ws
+    ws = skipMany space
+    numberfy num denom = Rational (read num % read denom)
+
+parseComplex :: Parser LispVal
+parseComplex = try $ numberfy <$> real <*> imag
+  where
+    real = read . fromMaybe "0" <$> (optionMaybe . try $ numFollowedBy '+')
+    imag = read <$> numFollowedBy 'i'
+    numberfy r i = Complex (r :+ i)
+    numFollowedBy c = (parseFloatString <|> many1 digit) <* char c
 
 
 parseExpr :: Parser LispVal
-parseExpr  = parseNumber
+parseExpr  = parseRational
+         <|> parseComplex
+         <|> parseFloat
+         <|> parseNumber
          <|> parseCharacter
-         <|> parseString
          <|> parseAtom
+         <|> parseString
+         <|> parseBool
